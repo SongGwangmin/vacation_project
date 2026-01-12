7d33c532d2517fe2501ba9b1115732964b4fd461
Gwangmin Song <thdalsrhkd@naver.com>
2026-01-12 23:14:29 +0900

    Add project files.

---
+++ vacation_project/main.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <vector>
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
