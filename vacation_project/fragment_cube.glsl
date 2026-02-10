fea8c7b30efed9901121568c7ebbdd9e1fe56b3f
Gwangmin Song <thdalsrhkd@naver.com>
2026-02-11 00:33:01 +0900

    하프 램버트 사용
    게다가 보조 광원 추가

--- vacation_project/fragment_cube.glsl
+++ vacation_project/fragment_cube.glsl
@@ -7,16 +7,22 @@ out vec4 FragColor;
 
 void main() {
     // 광원 위치
-    vec3 lightPos = vec3(1.0, 1.0, 1.0);
-    
+    vec3 lightPos = vec3(100.0, 100.0, 50.0);
+    vec3 Filllight = vec3(50.0, 50.0, -100.0);
     // 디퓨즈 계산
+
     vec3 lightDir = normalize(lightPos - vWorldPos);
     vec3 normal = normalize(vNormal);
-    float diff = max(dot(normal, lightDir), 0.3);
+    float diff = max(dot(normal, lightDir), 0.0);
+
+    float Filldiff = max(dot(normal, normalize(Filllight - vWorldPos)), 0.0);
+
+    //diff = diff * 0.5 + 0.5; // half lambert
     
     // 주변광 + 디퓨즈
     //float ambient = 0.3;
-    vec3 result = diff * vColor;
+    vec3 result = diff  * vColor;
+    result += Filldiff * 0.2 * vColor;
     
     FragColor = vec4(result, 1.0);
 }
