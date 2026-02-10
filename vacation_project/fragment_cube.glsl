8ae2c57f85829c4439a4e1d0a143bcf5d2df21a1
Gwangmin Song <thdalsrhkd@naver.com>
2026-02-11 00:19:24 +0900

    엠비언트 제거 후 디퓨즈 최솟값 설정

--- vacation_project/fragment_cube.glsl
+++ vacation_project/fragment_cube.glsl
@@ -12,11 +12,11 @@ void main() {
     // 디퓨즈 계산
     vec3 lightDir = normalize(lightPos - vWorldPos);
     vec3 normal = normalize(vNormal);
-    float diff = max(dot(normal, lightDir), 0.0);
+    float diff = max(dot(normal, lightDir), 0.3);
     
     // 주변광 + 디퓨즈
-    float ambient = 0.3;
-    vec3 result = (ambient + diff) * vColor;
+    //float ambient = 0.3;
+    vec3 result = diff * vColor;
     
     FragColor = vec4(result, 1.0);
 }
